3c94028192009595f8d06ec71488669ec233d772
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2023-07-08 07:51:49 +0100

    Drop Python 3.6 and 3.7, and bump dependencies (#49)

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -4,41 +4,20 @@ __all__ = ["extras_require"]
 
 extras_require = {
 		"pubchem": [
-				"cawdrey>=0.1.7",
-				"mathematical>=0.1.13",
-				'pillow>=7.0.0; platform_python_implementation == "PyPy" and python_version >= "3.8" and platform_system == "Windows"',
-				'pillow>=7.0.0; platform_python_implementation == "PyPy" and python_version >= "3.7" and platform_system != "Windows"',
-				'pillow>=7.0.0; platform_python_implementation != "PyPy"',
-				'pillow<9.0.0,>=7.0.0; platform_python_implementation == "PyPy" and python_version == "3.7" and platform_system == "Windows"',
-				'pillow<8.0.0,>=7.0.0; platform_python_implementation == "PyPy" and python_version == "3.6"',
-				"pyparsing>=2.4.6",
-				"tabulate>=0.8.9"
+				"cawdrey>=0.1.7", "mathematical>=0.1.13", "pillow>=9.0.0", "pyparsing>=2.4.6", "tabulate>=0.8.9"
 				],
-		"formulae": [
-				"cawdrey>=0.1.7",
-				"mathematical>=0.1.13",
-				"pyparsing>=2.4.6",
-				"tabulate>=0.8.9",
-				"typing-extensions>=4.1.1"
-				],
-		"plotting": [
-				'matplotlib>=3.0.0; platform_machine != "aarch64" or python_version > "3.6"',
-				'matplotlib<=3.2.2; platform_machine == "aarch64" and python_version == "3.6"'
-				],
+		"formulae": ["cawdrey>=0.5.0", "mathematical>=0.5.1", "pyparsing>=2.4.6", "tabulate>=0.8.9"],
+		"plotting": ["matplotlib>=3.6.0"],
 		"toxnet": ["beautifulsoup4>=4.7.0"],
 		"all": [
 				"beautifulsoup4>=4.7.0",
+				"cawdrey>=0.5.0",
 				"cawdrey>=0.1.7",
+				"mathematical>=0.5.1",
 				"mathematical>=0.1.13",
-				'matplotlib>=3.0.0; platform_machine != "aarch64" or python_version > "3.6"',
-				'matplotlib<=3.2.2; platform_machine == "aarch64" and python_version == "3.6"',
-				'pillow>=7.0.0; platform_python_implementation == "PyPy" and python_version >= "3.8" and platform_system == "Windows"',
-				'pillow>=7.0.0; platform_python_implementation == "PyPy" and python_version >= "3.7" and platform_system != "Windows"',
-				'pillow>=7.0.0; platform_python_implementation != "PyPy"',
-				'pillow<9.0.0,>=7.0.0; platform_python_implementation == "PyPy" and python_version == "3.7" and platform_system == "Windows"',
-				'pillow<8.0.0,>=7.0.0; platform_python_implementation == "PyPy" and python_version == "3.6"',
+				"matplotlib>=3.6.0",
+				"pillow>=9.0.0",
 				"pyparsing>=2.4.6",
-				"tabulate>=0.8.9",
-				"typing-extensions>=4.1.1"
+				"tabulate>=0.8.9"
 				]
 		}
